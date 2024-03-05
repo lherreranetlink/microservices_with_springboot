705c05a15d4386aa76f18fdb9a9e0026bd304947
Leonardo Herrera Ornelas <leonardoherreraornelas960302@gmail.com>
2024-03-05 16:49:27 -0600

    first commit

---
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/core/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package se.magnus.microservices.core.product;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
