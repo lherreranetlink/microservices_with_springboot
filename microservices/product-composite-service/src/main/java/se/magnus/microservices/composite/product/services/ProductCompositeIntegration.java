302144473ae55427d6f85d0f7cce76d571d6a76a
lherreranetlink <leonardoherreraornelas960302@gmail.com>
2024-07-02 23:27:02 -0600

    change typos referencing eureka server in docker-compose

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -60,16 +60,7 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
 
             WebClient.Builder webClient,
             ObjectMapper mapper,
-            StreamBridge streamBridge,
-
-            @Value("${app.product-service.host}") String productServiceHost,
-            @Value("${app.product-service.port}") int  productServicePort,
-
-            @Value("${app.recommendation-service.host}") String recommendationServiceHost,
-            @Value("${app.recommendation-service.port}") int  recommendationServicePort,
-
-            @Value("${app.review-service.host}") String reviewServiceHost,
-            @Value("${app.review-service.port}") int  reviewServicePort
+            StreamBridge streamBridge
     ) {
 
         this.publishEventScheduler = publishEventScheduler;
